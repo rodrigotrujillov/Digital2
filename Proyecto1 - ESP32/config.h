38bfd845046567a1fe7bf225cc0a748e83658d4b
Rodrigo Trujillo <62636431+rodrigotrujillov@users.noreply.github.com>
2023-08-30 15:35:39 -0600

    Update config.h

--- Proyecto1 - ESP32/config.h
+++ Proyecto1 - ESP32/config.h
@@ -2,11 +2,11 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME  // your name
-#define IO_KEY       //your key
+#define IO_USERNAME  "your name"
+#define IO_KEY       "your key"
 
-#define WIFI_SSID // your wifi
-#define WIFI_PASS // your wifipass
+#define WIFI_SSID  "your wifi"
+#define WIFI_PASS  "your wifipass"
 
 #include "AdafruitIO_WiFi.h"
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
